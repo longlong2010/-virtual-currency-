3f5e5ee0ffa433a0d6f0c4fa4474934715d50ede
Long Long <longlong2010@gmail.com>
2017-07-16 08:29:01 +0000

    add kline graphics

--- nts.py
+++ nts.py
@@ -3,6 +3,10 @@ import httplib2;
 import json;
 from urllib.parse import urlencode;
 import time;
+import matplotlib.pyplot;
+import matplotlib.finance;
+from matplotlib.pylab import date2num;
+import datetime;
 
 def kline(symbol, type):
     http = httplib2.Http();
@@ -15,9 +19,11 @@ def kline(symbol, type):
 
 if __name__ == '__main__':
     #比特币日K线数据
-    data = kline('btc_cny', '1day');
+    data = kline('btc_cny', '1week');
     n = len(data);
+
     for i in range(0, n):
+        data[i][0] = date2num(datetime.datetime.fromtimestamp(data[i][0] / 1000));
         if i >= 9 + 4:
             #买入结构，连续9次收盘价都低于四天前的收盘价
             for j in range(0, 9):
@@ -27,7 +33,7 @@ if __name__ == '__main__':
                 if x[4] > y[4]:
                     break;
             if j == 8:
-                print("Buy %s" % (time.strftime("%Y%m%d %H:%M:%S", time.gmtime(data[i][0] / 1000))));
+                print("Buy %s" % data[i][0]);
 
             #卖出结构，连续9次收盘价都高于四天前的收盘价
             for j in range(0, 9):
@@ -37,4 +43,8 @@ if __name__ == '__main__':
                 if x[4] < y[4]:
                     break;
             if j == 8:
-                print("Sell %s" % (time.strftime("%Y%m%d %H:%M:%S", time.gmtime(data[i][0] / 1000))));
+                print("Sell %s" % data[i][0]);
+    
+    _, ax = matplotlib.pyplot.subplots();
+    matplotlib.finance.candlestick_ohlc(ax, data);
+    matplotlib.pyplot.show();
